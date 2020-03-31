3d7f7fc304755b30c1efcf17e559d32d6e2e1901
MarieHeleneBury <mh.bury@imt-atlantique.net>
2020-03-31 13:42:14 +0200

    integrating Flask RESTful endpoints and SQLAlchemy

--- backend/src/entities/pokemon.py
+++ backend/src/entities/pokemon.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from marshmallow import Schema, fields
+
 from sqlalchemy import Column, String
 
 from .entity import Entity, Base
@@ -15,4 +17,13 @@ class Pokemon(Entity, Base):
         Entity.__init__(self, created_by)
         self.name = name
         self.elem_type = elem_type
+
+class PokemonSchema(Schema):
+    id = fields.Number()
+    name = fields.Str()
+    elem_type = fields.Str()
+    created_at = fields.DateTime()
+    updated_at = fields.DateTime()
+    last_updated_by = fields.Str()
+
 
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,28 +1,61 @@
 # coding=utf-8
 
+from flask import Flask, jsonify, request
+
 from .entities.entity import Session, engine, Base
-from .entities.pokemon import Pokemon
+from .entities.pokemon import Pokemon, PokemonSchema
+
+# creating the Flask application
+app = Flask(__name__)
 
 # generate database schema
 Base.metadata.create_all(engine)
 
-# start session
-session = Session()
 
-# check for existing data
-pokemons = session.query(Pokemon).all()
+@app.route('/pokemons')
+def get_pokemons():
+    # fetching from the database
+    session = Session()
+    pokemon_objects = session.query(Pokemon).all()
 
-if len(pokemons) == 0:
-    # create and persist dummy pokemon
-    python_pokemon = Pokemon("Bidoof", "Normal", "script")
-    session.add(python_pokemon)
-    session.commit()
+    # transforming into JSON-serializable objects
+    schema = PokemonSchema(many=True)
+    pokemons = schema.dump(pokemon_objects)
+
+    # serializing as JSON
     session.close()
+    return jsonify(pokemons)
 
-    # reload pokemons
-    pokemons = session.query(Pokemon).all()
+@app.route('/pokemons', methods=['POST'])
+def add_pokemon():
+    # mount pokemon object
+    posted_pokemon = PokemonSchema(only=('name', 'elem_type'))\
+        .load(request.get_json())
 
-# show existing pokemons
-print('### Pokemons:')
-for pokemon in pokemons:
-    print(f'({pokemon.id}) {pokemon.name} - {pokemon.elem_type}')
+    pokemon = Pokemon(**posted_pokemon, created_by="HTTP post request")
+
+    # persist pokemon
+    session = Session()
+    session.add(pokemon)
+    session.commit()
+
+    # return created pokemon
+    new_pokemon = PokemonSchema().dump(pokemon)
+    session.close()
+    return jsonify(new_pokemon), 201
+
+
+# if len(pokemons) == 0:
+#     # create and persist dummy pokemon
+#     python_pokemon = Pokemon("Bidoof", "Normal", "script")
+#     session.add(python_pokemon)
+#     session.commit()
+#     session.close()
+
+#     # reload pokemons
+#     pokemons = session.query(Pokemon).all()
+
+# # show existing pokemons
+# print('### Pokemons:')
+# for pokemon in pokemons:
+#     print(f'({pokemon.id}) {pokemon.name} - {pokemon.elem_type}')
