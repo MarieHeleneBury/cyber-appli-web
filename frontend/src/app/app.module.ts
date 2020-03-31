53d1fcab86e15058d08ef21bc2cf05e8e1593144
MarieHeleneBury <mh.bury@imt-atlantique.net>
2020-03-31 16:27:42 +0200

    clean from previous failed test

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import {PokemonsApiService} from './pokemons/pokemons-api.service';
+import { PokemonsApiService } from './pokemons/pokemons-api.service';
 
 @NgModule({
   declarations: [
--- frontend/src/app/pokemons/pokemons-api.service.ts
+++ frontend/src/app/pokemons/pokemons-api.service.ts
@@ -23,6 +23,5 @@ export class PokemonsApiService {
             return throwError(err);
           })
       )
-    //   .catch(PokemonsApiService._handleError);
   }
 }
